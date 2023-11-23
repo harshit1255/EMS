4af955bcb3164f1f675b4d57b152df4929041507
HARSHIT MISHRA <harshitmishra1255@gmail.com>
2023-11-23 23:50:07 +0530

    created element visible on table and added eventlistener for deletion

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const onsubmitform=(e)=>
     addemployee(employee);
 }
 form.addEventListener('submit',onsubmitform);
-
+// let span_button =null;
 function addemployee(employee)
 {
     let tr =document.createElement('tr');
@@ -33,5 +33,43 @@ function addemployee(employee)
     let td =document.createElement('td');
     td.innerHTML=employee.name;
     tr.appendChild(td);
-    console.log(td);
+    td =document.createElement('td');
+    td.innerHTML=employee.employeeId;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerHTML=employee.salary;
+    tr.appendChild(td);
+    td =document.createElement('td');
+    td.innerHTML=employee.team;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerHTML=employee.role;
+    tr.appendChild(td);
+    td=document.createElement('td');
+    let span = document.createElement('span');
+    span.className = 'material-icons';
+    // span_button=span;
+    span.innerHTML='delete';
+    td.appendChild(span);
+    span = document.createElement('span');
+    span.className = 'material-icons';
+    span.innerHTML='edit';
+    td.appendChild(span);
+    tr.appendChild(td);
+    
+    document.getElementById('table-body').appendChild(tr);
+    console.log(tr);
 }
+
+document.querySelector('#table-body').addEventListener('click',(e)=>
+{
+    console.log('hello')
+    console.log(e.currentTarget);
+    console.log(e.target);
+    if(e.target.innerHTML==='delete')
+        
+            console.log('delete pressed')
+        }
+)
+
+///***imp elem.classList.toggle('abc') abc will be aaded if present in present class list of elem or addded if not present */
